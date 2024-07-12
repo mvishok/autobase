3184351c2481adb7ea7106527ecbc050c5eea9f8
mvishok <vishokmanikantan@gmail.com>
2024-07-12 17:33:30 +0530

    chore: Update CSV file loader to filter out columns starting with "#"

--- pkg/loader/csv.go
+++ pkg/loader/csv.go
@@ -3,11 +3,13 @@ package loader
 import (
 	"encoding/csv"
 	"os"
-	"statix/pkg/log"
+	"syncengin/pkg/log"
+	"strings"
 )
 
 func ReadCSV(filePath string) ([][]string, error) {
 	log.Info("Reading CSV file: " + filePath)
+
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
@@ -15,5 +17,32 @@ func ReadCSV(filePath string) ([][]string, error) {
 	defer file.Close()
 
 	reader := csv.NewReader(file)
-	return reader.ReadAll()
+	allRows, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(allRows) == 0 {
+		log.Info("No rows found in CSV file: " + filePath)
+		return [][]string{}, nil
+	}
+
+	headers := allRows[0]
+	var validIndices []int
+	for i, header := range headers {
+		if !strings.HasPrefix(header, "#") {
+			validIndices = append(validIndices, i)
+		}
+	}
+
+	filteredRows := make([][]string, len(allRows))
+	for i, row := range allRows {
+		var filteredRow []string
+		for _, idx := range validIndices {
+			filteredRow = append(filteredRow, row[idx])
+		}
+		filteredRows[i] = filteredRow
+	}
+
+	return filteredRows, nil
 }
