152d03f6e7f0dc0c8165dfc22c821db118f9191c
mvishok <vishokmanikantan@gmail.com>
2024-07-20 17:26:21 +0530

    Refactor query package to improve performance and readability

--- pkg/core/query/query.go
+++ pkg/core/query/query.go
@@ -2,158 +2,119 @@ package query
 
 import (
 	"net/url"
-	"sort"
-	"strconv"
 	"strings"
+	"syncengin/pkg/loader"
 	"syncengin/pkg/log"
 )
 
-func Run(rows [][]string, query url.Values, access_level string) []map[string]string {
+var headers []string
+
+func Run(csvPath string, query url.Values, access_level string) []map[string]string {
+	rows, err := loader.ReadCSV(csvPath)
+	if err != nil {
+		log.Error("An error occurred while reading the CSV file: " + err.Error())
+		return nil
+	}
 	if len(rows) < 2 {
 		return nil
 	}
 
-	headers := rows[0]
 	results := []map[string]string{}
 
-	for _, row := range rows[1:] {
-		record := make(map[string]string)
-		for i, value := range row {
-			record[headers[i]] = value
+	//if empty query, return all rows
+	if len(query) == 0 {
+		for i, row := range rows {
+			if i == 0 {
+				continue
+			}
+			result := make(map[string]string)
+			for i, value := range row {
+				result[rows[0][i]] = value
+			}
+			results = append(results, result)
 		}
-		if rowMatchesQuery(record, query) {
-			results = append(results, record)
-		}
+		return results
 	}
 
-	//if it is select query
+	//if select query, return only selected columns
 	if query.Get("select") != "" {
-		if access_level == "read" || access_level == "write" {
-			results = selectFields(results, query.Get("select"))
-		}
-	} else {
-		//return empty
-		log.Error("Unkown query type")
-		return []map[string]string{}
-	}
 
-	results = orderBy(results, query.Get("order"))
+		cols := strings.Split(query.Get("select"), ",")
+
+		for i, row := range rows {
+			if i == 0 {
+				headers = row
+				continue
+			}
+			if whereClause(row, query) {
+				result := make(map[string]string)
+				for i, value := range row {
+					if contains(cols, rows[0][i]) {
+						result[rows[0][i]] = value
+					}
+				}
+				results = append(results, result)
+			}
+		}
+	}
 
 	return results
 }
 
-func selectFields(rows []map[string]string, fields string) []map[string]string {
-	if fields == "" {
-		return rows
-	}
-
-	selectedFields := strings.Split(fields, ",")
-	var selected []map[string]string
-
-	for _, row := range rows {
-		selectedRow := make(map[string]string)
-		for _, field := range selectedFields {
-			if value, ok := row[field]; ok {
-				selectedRow[field] = value
-			}
+func contains(arr []string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
 		}
-		selected = append(selected, selectedRow)
 	}
-
-	return selected
+	return false
 }
 
-func rowMatchesQuery(record map[string]string, query url.Values) bool {
-	where := query.Get("where")
-	if where == "" {
-		return true
-	}
+// whereClause returns true if the row satisfies the where clause
+func whereClause(row []string, query url.Values) bool {
+	for key, value := range query {
+		if key == "where" {
+			for _, v := range value {
+				//syntax: &where=key:op:val,key:op:val...
+				conditions := strings.Split(v, ",")
 
-	conditions := strings.Split(where, ",")
-	for _, condition := range conditions {
-		parts := strings.SplitN(condition, " ", 3)
-		if len(parts) != 3 {
-			return false
-		}
-		field, operator, value := parts[0], parts[1], parts[2]
+				for _, condition := range conditions {
+					//syntax: key:op:val
+					parts := strings.Split(condition, ":")
 
-		recordValue := record[field]
+					if len(parts) != 3 {
+						return false
+					}
 
-		switch operator {
-		case "eq":
-			if recordValue != value {
-				return false
+					key := parts[0]
+					op := parts[1]
+					val := parts[2]
+
+					index := getHeaderIndex(key)
+					if index == -1 {
+						return false
+					}
+
+					switch op {
+					case "eq":
+						if row[index] != val {
+							return false
+						}
+					default:
+						return false
+					}
+				}
 			}
-		case "neq":
-			if recordValue == value {
-				return false
-			}
-		case "gt":
-			recordFloat, err := strconv.ParseFloat(recordValue, 64)
-			queryFloat, err2 := strconv.ParseFloat(value, 64)
-			if err != nil || err2 != nil || recordFloat <= queryFloat {
-				return false
-			}
-
-		case "gte":
-			recordFloat, err := strconv.ParseFloat(recordValue, 64)
-			queryFloat, err2 := strconv.ParseFloat(value, 64)
-			if err != nil || err2 != nil || recordFloat < queryFloat {
-				return false
-			}
-
-		case "lt":
-			recordFloat, err := strconv.ParseFloat(recordValue, 64)
-			queryFloat, err2 := strconv.ParseFloat(value, 64)
-			if err != nil || err2 != nil || recordFloat >= queryFloat {
-				return false
-			}
-
-		case "lte":
-			recordFloat, err := strconv.ParseFloat(recordValue, 64)
-			queryFloat, err2 := strconv.ParseFloat(value, 64)
-			if err != nil || err2 != nil || recordFloat > queryFloat {
-				return false
-			}
-
-		case "contains":
-			if !strings.Contains(recordValue, value) {
-				return false
-			}
-
-		default:
-			return false
 		}
 	}
 	return true
 }
 
-func orderBy(rows []map[string]string, order string) []map[string]string {
-	if order == "" {
-		return rows
-	}
-
-	orderFields := strings.Split(order, ",")
-	sort.SliceStable(rows, func(i, j int) bool {
-		for _, field := range orderFields {
-			parts := strings.Split(field, " ")
-			fieldName := parts[0]
-			desc := false
-			if len(parts) > 1 && parts[1] == "desc" {
-				desc = true
-			}
-
-			if rows[i][fieldName] == rows[j][fieldName] {
-				continue
-			}
-
-			if desc {
-				return rows[i][fieldName] > rows[j][fieldName]
-			}
-			return rows[i][fieldName] < rows[j][fieldName]
+func getHeaderIndex(key string) int {
+	for i, h := range headers {
+		if h == key {
+			return i
 		}
-		return false
-	})
-
-	return rows
+	}
+	return -1
 }
