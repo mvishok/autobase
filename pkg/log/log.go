a08240c653f1beb1720508ef89ceeb412cdf850a
mvishok <vishokmanikantan@gmail.com>
2024-07-27 22:16:15 +0530

    Update log package to use ANSI escape codes for colored output

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -2,33 +2,41 @@ package log
 
 import (
 	"fmt"
-
-	"github.com/TwiN/go-color"
 )
 
 var enabled = true
 
+const Reset = "\033[0m"
+const Red = "\033[31m"
+const Green = "\033[32m"
+const Yellow = "\033[33m"
+const Blue = "\033[34m"
+const Magenta = "\033[35m"
+const Cyan = "\033[36m"
+const Gray = "\033[37m"
+const White = "\033[97m"
+
 func Info(message string) {
 	if enabled {
-		fmt.Println(color.InCyan("INFO: " + message))
+		fmt.Println(Cyan + "INFO: " + Reset + message)
 	}
 }
 
 func Error(message string) {
 	if enabled {
-		fmt.Println(color.InBold(color.InRed("ERROR: " + message)))
+		fmt.Println(Red + "ERROR: " + Reset + message)
 	}
 }
 
 func Success(message string) {
 	if enabled {
-		fmt.Println(color.InBold(color.InGreen("SUCCESS: " + message)))
+		fmt.Println(Green + "SUCCESS: " + Reset + message)
 	}
 }
 
 func Warning(message string) {
 	if enabled {
-		fmt.Println(color.InYellow("WARNING: " + message))
+		fmt.Println(Yellow + "WARNING: " + Reset + message)
 	}
 }
 
